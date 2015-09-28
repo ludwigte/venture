c03b475568e0aa669f6fc402a459a36f8f27204b
Tom Ludwig <tom.ludwig@gmail.com>
2015-09-28 22:30:05 +0000

    Update to Sept 26 version

--- app/models/character.js
+++ app/models/character.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+
 import Item from './item';
 const BASE_HP = 40;
 const BASE_MP = 30;
 
 export default DS.Model.extend({
-  level: DS.attr('number',{defaultValue: 1}),
+  level: DS.attr('number', {defaultValue: 1}),
   intelligence: 17,
   strength: 4,
   wisdom: 19,
@@ -16,34 +17,32 @@ export default DS.Model.extend({
     var classes = ['Wizard', 'Warrior', 'Bard'];
     return classes[Math.floor(Math.random()*classes.length)];
   }),
-
-  items: Ember.computed( function(){
-    return [Item.createRandom()];
-  }),
-
+  
+  items: DS.hasMany('items'),
+  
   maxHealth: Ember.computed('level', 'effectiveConstitution', function() {
     return BASE_HP + (this.get('effectiveConstitution') * this.get('level'));
   }),
-
+  
   maxMana: Ember.computed('level', 'intelligence', function() {
     return BASE_MP + (this.get('intelligence') * this.get('level'));
   }),
-
+  
   itemWeights: Ember.computed.mapBy('items','weight'),
   itemWeight: Ember.computed.sum('itemWeights'),
   hampered: Ember.computed('itemWeight','maxWeight', function(){
      return this.get('itemWeight') > this.get('maxWeight');
   }),
   maxWeight: Ember.computed('strength', function() {
-     return this.get('strength') * 5;
+     return this.get('strength') * 5; 
   }),
-
+  
   name: Ember.computed(function(){
     var names = ['Zultar', 'Zorky', 'Merlin'];
     return names[Math.floor(Math.random()*names.length)];
   }),
 
-  itemConstitutionBonuses: Ember.computed.mapBy('items','bonuses.constitution'),
+  itemConstitutionBonuses: Ember.computed.mapBy('items','constitutionBonus'),
   constitutionBonus: Ember.computed.sum('itemConstitutionBonuses'),
   effectiveConstitution: Ember.computed('constitutionBonus','constitution', function() {
     return this.get('constitution') + this.get('constitutionBonus');
